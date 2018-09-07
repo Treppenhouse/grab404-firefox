a3bd212c1a0388320cfb6bc0023269146a6afe13
MalteM <schrobe@gmail.com>
2018-09-07 12:36:15 +0200

    basic functionality working now

--- grab404.js
+++ grab404.js
@@ -1,7 +1,10 @@
 const { writeFile } = require('fs');
+const { mkdirp } = require('mkdirp');
+const getDirName = require('path').dirname;
 
-const {Builder, By, Key, promise, until} = require('selenium-webdriver');
+const {Builder, promise, until} = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
+const promiseUtil = require('selenium-webdriver/lib/promise');
 
 // The argument parser
 const {ArgumentParser, HelpFormatter} = require('argparse');
@@ -83,148 +86,74 @@ const driver = new Builder()
 console.log('Firefox initialized...')
 
 
-
 /**
- * Render 404 error pages of given urls
+ * Calculate the filename of the screenshot image based on the url and the current index
  * 
- * @param urls
- *            of URLs to render
- * @param callbackPerUrl
- *            Function called after finishing each URL, including the last URL
- * @param callbackFinal
- *            Function called after finishing everything
+ * @param url
+ *            the url
  */
-var render404Pages = function(urls, callbackPerUrl, callbackFinal) {
-    // screenshot dimensions
-    const width = 1024;
-    const height = 1200;
+var calculateOutputFilename = function(url) {
+    var indexString = ("0" + urlIndex).slice(-2);
+    strippedUrl = url.replace("/", "")
+    var outputFileName = './out/' + indexString + '.' + strippedUrl + '.png';
+    return outputFileName;
+};
 
-    var urlIndex = 0;
+let urlIndex = 0;
 
+var retrieveNext = function() {
+    var url;
+    if(urls.length < 1){
+    	afterLast();
+    	return;
+    }
+	url = urls.shift();
+	urlIndex++;
 
+	let outputFileName = calculateOutputFilename(url);
+	let outputDirectoryName = getDirName(outputFileName)
+	// TODO: why www here?
+	var fullUrl = "http://www." + url + "/reddit";
 
-    /**
-     * Calculate the filename of the screenshot image based on the url and the current index
-     * 
-     * @param url
-     *            the url
-     */
-    var calculateOutputFilename = function(url) {
-        var indexString = ("0" + urlIndex).slice(-2);
-        strippedUrl = url.replace("/", "")
-        var outputFileName = "../out/" + indexString + "." + strippedUrl + ".png";
-        return outputFileName;
-    };
+	console.log('BEFORE GET...'+urls);
 
-
-    /**
-     * Calculate the filename of the screenshot image based on the url and the current index
-     * 
-     * @param url
-     *            the url
-     */
-    var closeCurrentAndRetrieveNext = function(url, file) {
-        callbackPerUrl(url, file);
-        return retrieveNext();
-    };
-
-    var retrieveNext = function() {
-        var url;
-        if (urls.length > 0) {
-            url = urls.shift();
-            
-            let outputFileName = calculateOutputFilename(url);
-            console.log("Outputfilename = " + outputFileName);
-            
-
-            // TODO: do they have to start with www?
-            var fullUrl = "http://www." + url + "/reddit";
-            urlIndex++;
-
-
-
-			var status = driver.get(fullUrl).then(element => {
-				console.log(element);
-				driver.takeScreenshot()
-				  	.then(data => {
-				  		console.log("screenshot taken?");
-				    	writeFile(outputFileName,data,'base64', () => {
-				    		console.log("File written: " + outputFileName);
-	   						closeCurrentAndRetrieveNext(fullUrl, outputFileName);
-				    	});
-				  	});
+	driver.get(fullUrl)
+	.then(
+		success => {
+			return driver.takeScreenshot()
+		},
+		error => {
+			console.error('ERROR GETTING THE URL: '+error);
+			retrieveNext();
+		})
+	.then(
+		data => {
+			mkdirp(outputDirectoryName,	function (error) {
+			    if (error){
+					console.error('ERROR CREATING DIRECTORY: '+error);
+					retrieveNext();
+					return;
+			    } 
+		    	writeFile(outputFileName, data, 'base64', (error) => {
+					if(error) {
+	  					console.error('ERROR WRITING FILE: '+error);
+	  				} else { 
+	  					console.log('FILE WRITTEN '+outputFileName)
+	  				}
+	  				retrieveNext();
+				});
 			});
-			
+		},
+		error => {
+			console.error('ERROR TAKING SCREENSHOT: '+error);
+			retrieveNext();
+		});
+	}
 
-            /*page = webpage.create();
-            page.settings.userAgent = "Grab 404 bot";
-            page.settings.resourceTimeout = 300000; // 5 minutes
-            page.onResourceTimeout = function(e) {
-                // the url whose request timed out
-                console.log("Timeout on URL: " + e.url + " Index = " + (index + 1));
-            };
-            page.viewportSize = {
-                width: width,
-                height: height
-            };
-            console.log("Requesting URL: " + fullUrl);
-            return page.open(fullUrl, function(status) {
-                page.evaluate(function(w, h) {
-                    document.body.bgColor = 'white';
-                    document.body.style.width = w + "px";
-                    document.body.style.height = h + "px";
-                }, width, height);
-                page.clipRect = {
-                    height: height,
-                    width: width
-                };
-                var outputFileName;
-                outputFileName = calculateOutputFilename(url);
-                console.log("Outputfilename = " + outputFileName);
-                if (status === "success") {
-                    return window.setTimeout((function() {
-                        page.render(outputFileName);
-                        console.log("File written: " + outputFileName);
-                        return closeCurrentAndRetrieveNext(status, fullUrl, outputFileName);
-                    }), 400);
-                } else {
-                    return closeCurrentAndRetrieveNext(status, fullUrl, outputFileName);
-                }
-            });*/
-        } else {
-            return callbackFinal();
-        }
-    };
-    return retrieveNext();
+retrieveNext();
+
+function afterLast() {
+	console.log('Quitting driver...');
+	driver.quit();
 }
 
-
-
-
-
-
-
-
-
-
-render404Pages(urls, (function(url, file) {
-    console.log("Rendered '" + url + "' at '" + file + "'");
-}), function() {
-	console.log('Exiting program...');
-	driver.quit();
-    process.exit();
-});
-
-console.log("hello");
-
-
-
-
-
-
-
-
-process.exit();	
-
-
-  
